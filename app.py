c658eae2b4040aa1120405d69b7fb25109a5b032
ShadowKing <98453184+ShadowKing007@users.noreply.github.com>
2023-09-10 22:09:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import sqlite3
 import json
-
-app = Flask(__name__,template_folder='templates')
+from sqlite3 import Cursor
+app = Flask(__name__, static_url_path='/static',template_folder='templates')
 # Serve static files (CSS, JS, images, etc.) from the 'static' folder
 app.static_folder = 'static'
 
@@ -59,11 +59,53 @@ def index():
         recipe['Ingredient-count']
     ))  
     # Fetch data from the 'recipes' table
+    num_recipes_to_display = 30  # Number of recipes to display
+    recipes_to_display = recipes_data[:num_recipes_to_display]
+
     # Close the database connection
     conn.commit()
     conn.close()
 
-    return render_template('index.html', recipes_data=recipes_data)
+    return render_template('index.html', recipes_data=recipes_to_display)
+
+#searching of recipes
+
+@app.route('/search')
+def search_recipe():
+    query = request.args.get('q')
+    print(f'Searching for: {query}')
+    app.logger.info(f"Received query: {query}")
+    conn = sqlite3.connect('myrecipe.db')
+    cursor = conn.cursor()
+    # Perform a database query based on the search query
+    cursor.execute("SELECT DISTINCT TranslatedRecipeName, ImageURL, TranslatedIngredients, TotalTimeInMins, Cuisine, TranslatedInstructions, URL, CleanedIngredients, IngredientCount FROM recipes WHERE TranslatedRecipeName LIKE ? LIMIT 30", ('%' + query + '%',))
+
+    results = cursor.fetchall()
+
+    # Convert the results to a list of dictionaries
+    recipes = []
+    for (
+        TranslatedRecipeName, ImageURL, TranslatedIngredients,
+        TotalTimeInMins, Cuisine, TranslatedInstructions, URL,
+        CleanedIngredients, IngredientCount
+    ) in results:
+        recipe = {
+            'name': TranslatedRecipeName,
+            'image_url': ImageURL,
+            'ingredients': TranslatedIngredients,
+            'total_time': TotalTimeInMins,
+            'cuisine': Cuisine,
+            'instructions': TranslatedInstructions,
+            'url': URL,
+            'cleaned_ingredients': CleanedIngredients,
+            'ingredient_count': IngredientCount,
+        }
+        recipes.append(recipe)
+
+    app.logger.info(f"Found {len(recipes)} matching recipes")
+
+    return jsonify(recipes)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
